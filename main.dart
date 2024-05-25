35c95b24aa7d1797a23f5c073c008c48c2e56733
Wilsons-Navid <wadotiwawil@gmail.com>
2024-05-25 13:01:05 +0300

    List

--- main.dart
+++ main.dart
@@ -14,4 +14,35 @@ void main() {
   print("Age is $age");
   print("Height is $height");
   print("Married Status is $isMarried");
+/*Data types*/
+//everything to so with number
+  int num1 = 10;
+  double num2 = 130.2;
+  num num3 = 50;
+  num num4 = 50.4;
+
+  num sum = num1 + num2 + num3 + num4;
+
+  print(sum);
+
+  //everything to do with text
+  String schoolName = "Powerlearn Project Academy";
+  String continent = "AFRICA";
+
+  print("My school name is $schoolName and the address is $address");
+
+//anything to know about Boleans
+
+bool isSingle = false;
+print("Married Status: $isSingle");
+ //List
+
+  List<String> names = ['john', 'James', 'Peter'];
+  print("Value of names is $names");
+  print("Value of names[0] is ${names[0]}");
+  print(names);
+ 
+
+
+
 }
