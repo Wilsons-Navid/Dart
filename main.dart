afbbd9f03727ebfca01be6cead5c78be5f343877
Wilsons-Navid <wadotiwawil@gmail.com>
2024-05-25 10:17:40 +0300

    Variables

---
+++ main.dart
@@ -0,0 +1,17 @@
+//variables
+
+void main() {
+  String name = "Wilson";
+  String address = "Cameroon";
+  num age = 21;
+  num height = 5.9;
+  bool isMarried = false;
+
+  //printing variables value
+
+  print("Name is $name");
+  print("Address is $address");
+  print("Age is $age");
+  print("Height is $height");
+  print("Married Status is $isMarried");
+}
